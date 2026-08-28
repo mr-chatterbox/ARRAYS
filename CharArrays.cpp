becd4b50dda9ecc9424518e1224dc282eed05a2a
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-28 11:54:51 +0530

    Character Arrays

---
+++ CharArrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char name[10];
+
+    cout << "Enter your name" << endl;
+    cin >> name;
+
+    cout << "Your name is ";
+    cout << name << endl;
+
+    return 0;
+
+}
